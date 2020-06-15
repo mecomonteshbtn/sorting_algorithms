c6fbaa239b4c920e91e3ad04a2ff10ab830a6438
MiguelMontoya-R <miguelmr0510@gmail.com>
2020-06-15 21:26:43 +0000

    Conditional for NULL and size == 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,6 +11,8 @@ void bubble_sort(int *array, size_t size)
 	size_t i = 0, j = 0;
 	int aux = 0;
 
+	if (array == NULL || size == 0)
+		return;
 	for (; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
